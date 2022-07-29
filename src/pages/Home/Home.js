8249acfd233794dbed7ab11c433b92f17f6a1e9e
mdbappa0001 <abutanif366@gmail.com>
2022-07-29 14:06:02 +0600

    home added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import PageTitle from '../../components/PageTitle';
+import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
+import ChooseUs from './ChooseUs';
+import Features from './Features/Features';
+import GetInTouch from './GetInTouch';
+import ProudSection from './ProudSection';
+import ReviewSection from './ReviewSection';
+import Tools from './Tools';
 
 const Home = () => {
     return (
         <div>
-            <h2>Thsi is for home</h2>
+            <PageTitle title=""></PageTitle>
+            <Banner />
+            <Features/>
+            <Tools />
+            <ChooseUs/>
+            <ProudSection/>
+            <BusinessSummary/>
+            <ReviewSection/>
+            <GetInTouch/>
+            <Footer />
         </div>
     );
 };
