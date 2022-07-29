9a33c4699b02c7e71fc6676d29b8ee4c762e7dbb
mdbappa0001 <abutanif366@gmail.com>
2022-07-29 14:02:24 +0600

    tool added

--- src/App.js
+++ src/App.js
@@ -3,32 +3,102 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './pages/About/About';
 import Login from './pages/Account/Login';
-import Signup from './pages/Account/Signup';
 import Blog from './pages/Blog/Blog';
 import Contact from './pages/Contact/Contact';
 import Home from './pages/Home/Home';
 import { Toaster } from 'react-hot-toast';
 import NotFound from './components/NotFound';
+import RequireAuth from './pages/Account/RequireAuth';
+import ToolDetail from './pages/Home/ToolDetail';
+import Dashboard from './pages/Dashboard/Dashboard';
+import MyProfile from './pages/Dashboard/Profile/MyProfile';
+import MyOrder from './pages/Dashboard/Order/MyOrder';
+import ReviewHere from './pages/Dashboard/Review/ReviewHere';
+import AllUsers from './pages/Dashboard/Users/AllUsers';
+import AddProduct from './pages/Dashboard/Product/AddProduct';
+import ManageOrders from './pages/Dashboard/Order/ManageOrders';
+import ManageProducts from './pages/Dashboard/Product/ManageProducts';
+import RequireAdmin from './pages/Account/RequireAdmin';
+import EditProfileInfo from './pages/Dashboard/Profile/EditProfileInfo';
+import Payment from './pages/Dashboard/Order/Payment';
+import MyPortfolio from './pages/MyPortfolio/MyPortfolio';
+import QuickResponse from './pages/Home/Features/QuickResponse';
+import Planning from './pages/Home/Features/Planning';
+import Analysis from './pages/Home/Features/Analysis';
+import Strategy from './pages/Home/Features/Strategy';
+import ManageReviews from './pages/Dashboard/Review/ManageReviews';
+import Signup from './pages/Account/Signup';
 
-function App() {
-  return (
-    <div>
+
+const App = () => {
+    // const ref = useRef()
+    // const location = useLocation();
+    // useEffect(() => {
+    //     ref.current.scrollIntoView({ behavior: "smooth" });
+    // }, [location]);
+
+    return (
         <Navbar>
-            
+                {/* <div ref={ref}></div> */}
+
                 <Routes >
-                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/' element={<Home />}>
+                        <Route path='/' element={<QuickResponse />}></Route>
+                        <Route path='/planning' element={<Planning />}></Route>
+                        <Route path='/analysis' element={<Analysis />}></Route>
+                        <Route path='/strategy' element={<Strategy />}></Route>
+                    </Route>
+                    <Route path='/home' element={<Home />}></Route>
+                    <Route path='/tool/:toolId' element={
+                        <RequireAuth><ToolDetail /></RequireAuth>
+                    }></Route>
                     <Route path='/blog' element={<Blog />}></Route>
                     <Route path='/about' element={<About />}></Route>
                     <Route path='/contact' element={<Contact />}></Route>
+                    <Route path='/portfolio' element={<MyPortfolio />}></Route>
                     <Route path='/login' element={<Login />}></Route>
                     <Route path='/signup' element={<Signup />}></Route>
+
+                    {/* dashboard start */}
+
+                    <Route path='/dashboard' element={<RequireAuth><Dashboard /></RequireAuth>}>
+                        <Route path='my-profile' element={<MyProfile />}></Route>
+                        <Route path='my-profile/:profileId' element={<EditProfileInfo />}></Route>
+                        <Route path='my-order' element={<MyOrder />}></Route>
+                        <Route path="payment/:orderId" element={<Payment />}></Route>
+                        <Route path='add-review' element={<ReviewHere />}></Route>
+
+                        {/* admin */}
+
+                        <Route path='users' element={<RequireAdmin>
+                            <AllUsers />
+                        </RequireAdmin>}></Route>
+                        <Route path='add-product' element={<RequireAdmin>
+                            <AddProduct />
+                        </RequireAdmin>}></Route>
+                        <Route path='manage-order' element={<RequireAdmin>
+                            <ManageOrders />
+                        </RequireAdmin>}></Route>
+                        <Route path='manage-product' element={<RequireAdmin>
+                            <ManageProducts />
+                        </RequireAdmin>}></Route>
+                        <Route path='manage-reviews' element={<RequireAdmin>
+                            <ManageReviews />
+                        </RequireAdmin>}></Route>
+
+                        {/* admin */}
+
+                    </Route>
+
+
+                    {/* dashboard end */}
+
+
                     <Route path='*' element={<NotFound />}></Route>
                 </Routes>
                 <Toaster />
-            
         </Navbar>
-    </div>
-);
+    );
 };
 
 export default App;
---
+++ src/pages/Home/Tool.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsCartCheck } from 'react-icons/bs';
+
+const Tool = ({ tool }) => {
+    const { img, name, des, _id, minOrder, available, price, ratings } = tool
+    return (
+        <div className="card lg:max-w-lg bg-base-100 ">
+            <figure><img className='w-full' src={img} alt="Pipes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name.slice(0, 20)}</h2>
+                <p className='pb-2'>{des.slice(0, 100) + '...'}</p>
+                <div className="flex flex-col md:flex-row ">
+                    <p className='font-bold'>Min Order : <span className='text-[#2a6ee4]'>{minOrder}</span></p>
+                    <p className='font-bold'>Price : $ <span className='text-[#2a6ee4]'>{price}<small>/ps</small></span></p>
+                </div>
+                <div className="flex flex-col md:flex-row">
+                    <p className='font-bold'>Available : <span className='text-[#2a6ee4]'>{available}</span></p>
+                    <p className='font-bold'>Ratings : <span className='text-[#2a6ee4]'>{ratings}</span></p>
+                    
+                </div>
+                <div className="card-actions pt-3 ">
+                    <Link to={'/tool/' + _id} className='flex btn text-white btn-primary items-center'>
+                        <p className='pr-3'> place order</p>
+                        <span className='text-white text-lg'><BsCartCheck /></span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tool;
