daf3e76e3a14343b8b4b3d2f6473d579b7d4c7dc
mdbappa0001 <abutanif366@gmail.com>
2022-07-29 13:56:39 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './pages/About/About';
+import Login from './pages/Account/Login';
+import Signup from './pages/Account/Signup';
+import Blog from './pages/Blog/Blog';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import { Toaster } from 'react-hot-toast';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div>
+        <Navbar>
+            
+                <Routes >
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/blog' element={<Blog />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                    <Route path='/contact' element={<Contact />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                    <Route path='/signup' element={<Signup />}></Route>
+                    <Route path='*' element={<NotFound />}></Route>
+                </Routes>
+                <Toaster />
+            
+        </Navbar>
     </div>
-  );
-}
+);
+};
 
 export default App;
---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Thsi si for aabout</h2>
+        </div>
+    );
+};
+
+export default Contact;
