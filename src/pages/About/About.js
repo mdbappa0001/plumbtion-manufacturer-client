3d0a318c456ed93f37937bb2465d229cffa4117c
mdbappa0001 <abutanif366@gmail.com>
2022-07-29 14:33:34 +0600

    about added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import construction from '../../assets/images/construction.png'
+import Footer from '../../components/Footer';
+import PageTitle from '../../components/PageTitle';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is for about section</h2>
+        <div className='px-5'>
+            <PageTitle title="About-us -"></PageTitle>
+            <img src={construction} alt="construction" />
+            <Footer />
         </div>
     );
 };
